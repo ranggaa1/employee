48e3c307e541445541f844b26c786007f509011b
Rangga <rangggagarang1@gmail.com>
2023-02-03 18:03:04 +0700

    balik

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -54,7 +54,7 @@
                                         <nav class="sb-sidenav-menu-nested nav">
                                         @if(isset(auth()->user()->role->permission ['name']['leave']['can-list']))
                                         <a class="nav-link" href="{{route('leaves.index')}}">
-                                            List Leaves tttttt
+                                            List Leaves
                                         </a>
                                         @endif
                                         <a class="nav-link" href="{{route('leaves.create')}}">
